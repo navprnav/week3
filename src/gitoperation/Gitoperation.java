6a8e776ac541947c5754fe89fd981680dde2da97
navprnav <97628424+navprnav@users.noreply.github.com>
2022-01-26 13:03:33 -0500

    Update Gitoperation.java

--- src/gitoperation/Gitoperation.java
+++ src/gitoperation/Gitoperation.java
@@ -7,6 +7,7 @@ public class Gitoperation {
     public static void main(String[] args) {
         System.out.println("github");
         System.out.println("navpreet");
+        System.out.println("Github is done");
     }
     
 }
